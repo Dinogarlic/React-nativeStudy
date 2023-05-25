d860d4179e364f52a58fd80b40e79c09be48bfdf
Dinogarlic <88885167+Dinogarlic@users.noreply.github.com>
2023-05-25 15:53:47 +0900

    Hello 출력 예제

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+const Hello = () => {
+	return (
+		<View>
+			<Text>Hello World!</Text>
+			<Text>Hello React Native~👋</Text>
+		</View>
+	)
+}
+
+export default Hello
